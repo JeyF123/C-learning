5bbdf9dbd96cab64f62be15a0a90a5730537d35e
JeyF123 <73453861+JeyF123@users.noreply.github.com>
2026-01-30 15:05:40 +0000

    Update main.cpp

--- Projekte/Haushaltsbuch/main.cpp
+++ Projekte/Haushaltsbuch/main.cpp
@@ -2,7 +2,16 @@
 #include <string>
 #include <map>
 class Datum_Text{
-    
+private:
+    std::string datum;
+    std::string text;
+public:
+    Datum_Text(std::string p_datum,std::string p_text):datum(p_datum),text(p_text){}
+
+    void zeigeBuchung(){
+        std::cout << datum << " " << text;
+    }
+
 };
 
 
@@ -12,26 +21,32 @@ int main(){
     std::cout << "2. Eine Buchung suchen\n";
     std::cout << "3. Buchungen auflisten\n";
     std::cout << "4. Eine Programm beenden\n";
-    
     int auswahl;
-    
     std::cin >> auswahl;
-    
+
+    std::map<Datum_Text,float>buchung;
+
     switch (auswahl){
+
         case 1:
-            Datum_Text neueBuchung;
+            std::string b_datum;
+            std::string b_text;
+            std::cout << "Datum: ";
+            std::cin >> b_datum;
+            std::cout << "Buchungstext: ";
+            std::cin >> b_text;
+
+            Datum_Text neueBuchung(b_datum,b_text);
             float betrag;
-            std::map<Datum_Text,float>buchung;
+
             break;
-    
         // case 2:
-        //     std::cout << "{Buchung suchen}";
-        // break;
-      
+        //     Datum_Text zeige;
+        //     zeige.zeigeBuchung();
+        //     break;
         // case 3:
         //     std::cout << "{Buchungen auflisten}";
         //     break;
-        
         // case 4:
         //     std::cout << "{Programm beenden}";
         //     break;
