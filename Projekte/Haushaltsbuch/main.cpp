591a7c9f6b7566686916d681eb7e43d58ae5f4e4
JeyF123 <73453861+JeyF123@users.noreply.github.com>
2026-01-30 14:07:55 +0000

    Update main.cpp

--- Projekte/Haushaltsbuch/main.cpp
+++ Projekte/Haushaltsbuch/main.cpp
@@ -12,20 +12,26 @@ int main(){
     std::cout << "2. Eine Buchung suchen\n";
     std::cout << "3. Buchungen auflisten\n";
     std::cout << "4. Eine Programm beenden\n";
+    
     int auswahl;
+    
     std::cin >> auswahl;
+    
     switch (auswahl){
         case 1:
             Datum_Text neueBuchung;
             float betrag;
             std::map<Datum_Text,float>buchung;
             break;
+    
         // case 2:
         //     std::cout << "{Buchung suchen}";
         // break;
+      
         // case 3:
         //     std::cout << "{Buchungen auflisten}";
         //     break;
+        
         // case 4:
         //     std::cout << "{Programm beenden}";
         //     break;
