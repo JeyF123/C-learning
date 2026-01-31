961e294fa72707f0fa82e23e4f49accee6268ff3
JeyF123 <73453861+JeyF123@users.noreply.github.com>
2026-01-31 02:13:01 +0000

    Update main1_2.cpp

--- Projekte/Haushaltsbuch/main1.cpp
+++ Projekte/Haushaltsbuch/main1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
+
 //gesamte Buchung als Klasse
 
 class Buchung{
@@ -9,29 +10,47 @@ private:
     std::string text;
     float betrag;
 public:
-    Buchung (std::string p_datum,std::string p_text, float b_betrag):datum(p_datum),text(p_text),betrag(b_betrag){}
+    Buchung (std::string p_datum,std::string p_text, float b_betrag):datum(p_datum),text(p_text),betrag(b_betrag){
+    }
 
     void zeigeBuchung(){
-        std::cout << datum << " " << text << " " << betrag;
+        std::cout << datum << " " << " " << text << " " << betrag << "\n";
     }
 
 };
 
+void menue(){
 
-int main(){
     std::cout << "H A U S H A L T S B U C H\n\nBitte wählen:\n\n";
     std::cout << "1. Eine Buchung hinzufügen\n";
-    std::cout << "2. Eine Buchung suchen\n";
-    std::cout << "3. Buchungen auflisten\n";
+    std::cout << "2. Buchungen auflisten\n";
+    std::cout << "3. Buchung suchen\n";
     std::cout << "4. Eine Programm beenden\n";
+}
+
+void clearScreen(){
+    for (int x = 0;x<100;x++){
+        std::cout << "\n";
+    }
+}
+
+
+
+int main(){
+
+    std::vector<Buchung> alleBuchungen;
+    while (true){
+    menue();
+
     int auswahl;
     std::cin >> auswahl;
-
-
 
     switch (auswahl){
 
         case 1:
+{
+            clearScreen();
+            std::cout << "Buchung eintragen: \n\n";
             std::string b_datum;
             std::string b_text;
             float b_betrag;
@@ -41,23 +60,34 @@ int main(){
             std::cin >> b_text;
             std::cout << "Betrag: ";
             std::cin >> b_betrag;
-            Buchung neueBuchung(b_datum,b_text,b_betrag);
+            Buchung eintrag(b_datum, b_text, b_betrag);
 
+            alleBuchungen.push_back(eintrag);
 
             break;
 
+}
+
+        case 2:
+            clearScreen();
+            std::cout << "Anzahl Buchungen: " << alleBuchungen.size() << "\n";
+            for(Buchung x : alleBuchungen){
+                x.zeigeBuchung();
+
+            }
 
 
-        // case 2:
-        //     Buchung zeige;
-        //     zeige.zeigeBuchung();
-        //     break;
-        // case 3:
-        //     std::cout << "{Buchungen auflisten}";
-        //     break;
-        // case 4:
-        //     std::cout << "{Programm beenden}";
-        //     break;
+
+
+            break;
+        case 3:
+            std::cout << "{Buchungen suchen}";
+            break;
+        case 4:
+            std::cout << "{Programm beenden}";
+            return 0;
+
+    }
     }
 
 }
