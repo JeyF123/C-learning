1a717b1f480563b75719208b90aa870051a5d5dc
JeyF123 <73453861+JeyF123@users.noreply.github.com>
2026-02-01 15:30:41 +0000

    Update main.cpp

--- Projekte/Tagebuch/main.cpp
+++ Projekte/Tagebuch/main.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <fstream>
+
+class Tagebuch {
+private:
+    std::string datum;
+    std::string text;
+
+public:
+    Tagebuch() {}   // leerer Start
+
+    void eintrag_datum() {
+        std::cout << "Bitte gib das Datum ein: ";
+        std::getline(std::cin, datum);
+    }
+
+    void eintrag_text() {
+        std::cout << "Bitte gib den Text ein: ";
+        std::getline(std::cin, text);
+    }
+    void ausgabe(){
+
+        std::ofstream datei ("tagebuch.txt");
+        for(char& x:text){
+            x+=37;
+        }
+        datei << datum << "\n" << text;
+        datei.close();
+    }
+    void entschluesseln() {
+        for (char &x : text) {
+            x -= 37;
+        }
+    }
+
+
+};
+
+int main(){
+    Tagebuch benutzer;
+    benutzer.eintrag_datum();
+    benutzer.eintrag_text();
+    benutzer.ausgabe();
+}
 
