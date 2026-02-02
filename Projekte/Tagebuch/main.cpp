d5876e39e2775e02afeb2320de60730fbdca453c
JeyF123 <73453861+JeyF123@users.noreply.github.com>
2026-02-02 02:04:58 +0000

    Update main.cpp

--- Projekte/Tagebuch/main.cpp
+++ Projekte/Tagebuch/main.cpp
@@ -18,28 +18,62 @@ public:
         std::cout << "Bitte gib den Text ein: ";
         std::getline(std::cin, text);
     }
-    void ausgabe(){
 
-        std::ofstream datei ("tagebuch.txt");
-        for(char& x:text){
-            x+=37;
-        }
-        datei << datum << "\n" << text;
-        datei.close();
-    }
-    void entschluesseln() {
+    void ausgabe() {
+        std::ofstream datei("Tagebuch.txt", std::ios::app);
         for (char &x : text) {
-            x -= 37;
+            x += 37;
         }
+        datei << datum << "\n" << text << "\n\n";
     }
 
 
+
+    void lesen(const std::string& dateiname) {
+        std::ifstream file(dateiname);
+
+        while (std::getline(file, datum)) {
+            std::getline(file, text);
+
+            for (char &x : text) {
+                x -= 37;
+            }
+
+            std::cout << datum << "\n";
+            std::cout << text << "\n\n";
+        }
+    }
 };
+
+void menue(int x){
+    std::cout << "1. Ins Tagebuch schreiben\n";
+    std::cout << "2. Vom Tagebuch lesen\n\n\n";
+}
 
 int main(){
     Tagebuch benutzer;
-    benutzer.eintrag_datum();
-    benutzer.eintrag_text();
-    benutzer.ausgabe();
-}
+    int auswahl;
+     menue(auswahl);
+    std::cin >> auswahl;
+    std::cin.ignore();
 
+    switch(auswahl){
+
+        case 1: {
+            benutzer.eintrag_datum();
+            benutzer.eintrag_text();
+            benutzer.ausgabe();
+            std::cout << "Eintrag gespeichert!";
+            break;
+        }
+
+    case 2:
+        benutzer.lesen("Tagebuch.txt");
+        break;
+
+    default:
+        std::cout << "Nur Tasten 1 oder 2 möglich.";
+        break;
+    }
+
+}
