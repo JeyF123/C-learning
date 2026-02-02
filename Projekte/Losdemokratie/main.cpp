2aa033fa7f15eaa86714592a72621f980206ae2a
JeyF123 <73453861+JeyF123@users.noreply.github.com>
2026-02-02 22:23:28 +0000

    Update main.cpp

--- Projekte/Losdemokratie/main.cpp
+++ Projekte/Losdemokratie/main.cpp
@@ -1,1 +1,36 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void benutzerID(int& p_benutzerID){
+    std::cout << "Bitte die BenutzerID eingeben\n";
+    std::cin >> p_benutzerID;
+}
+
+void stati (std::string& p_stati){
+    std::cout << "Bitte die Stati in 0 und 1 eingeben\n";
+    std::cin >> p_stati;
+}
+
+
+int main(){
+    int b_benutzerID;
+    benutzerID(b_benutzerID);
+
+    std::string b_stati;
+    stati(b_stati);
+
+    std::map<int,std::string> eingabe;
+    eingabe[b_benutzerID] = b_stati;
+
+    std::cout << eingabe[b_benutzerID] << "\n";
+    std::ofstream datei("Los.txt", std::ios::app);
+
+    for (const auto& eintrag:eingabe){
+        datei << eintrag.first << " " << eintrag.second << "\n";
+
+    }
+    return 0;
+}
 
